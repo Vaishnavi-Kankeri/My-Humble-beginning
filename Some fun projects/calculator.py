11a1699e9eb1520b8e18caa5d4ebd23dc8cea0c6
Vaishnavi-Kankeri <vrkankeri@gmail.com>
2025-04-15 23:18:13 +0530

    Add files via upload

---
+++ Some fun projects/calculator.py
@@ -0,0 +1,25 @@
+def add(x, y):
+  answer = x + y
+  return answer
+
+def subtract(x, y):
+  answer = x - y
+  return answer
+
+def multiply(x, y):
+  answer = x * y
+  return answer
+
+def divide(x, y):
+  answer = x // y
+  return answer
+
+def exp(x, y):
+  answer = x ** y
+  return answer
+
+print(add(3, 4))          # Same thing as print(7)
+print(multiply(1, 5))          # Same thing as print(5)
+print(subtract(5, 3))          # Same thing as print(2)
+print(divide(6, 3))          # Same thing as print(2)
+print(exp(3, 4))          # Same thing as print(81)
